c60bcb837c0e6c8a861550f537c7fd886f8ea042
Nguyen Van Binh <binhnv@local.com>
2023-02-02 22:21:31 +0700

    fix: ecr authenticate error

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "this" {}
 
+data "aws_ecr_authorization_token" "token" {}
+
+provider "docker" {
+  registry_auth {
+    address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.name)
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
+
 locals {
   ecr_address    = coalesce(var.ecr_address, format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.name))
   ecr_repo       = var.create_ecr_repo ? aws_ecr_repository.this[0].id : var.ecr_repo
